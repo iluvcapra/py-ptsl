2cfbebecd8d189fbd83a3b34d5625ec5c4855e5a
Jamie Hardt <jamiehardt@me.com>
2023-01-07 10:56:33 -0800

    Fixing imports

--- ptsl/client.py
+++ ptsl/client.py
@@ -1,6 +1,6 @@
 import grpc
-import PTSL_pb2_grpc
-from PTSL_pb2 import *
+from . import PTSL_pb2_grpc
+from . import PTSL_pb2 as p
 
 PTSL_VERSION=1
 
@@ -8,15 +8,17 @@ def run():
     with grpc.insecure_channel('localhost:0000') as channel:
         stub = PTSL_pb2_grpc.PTSLStub(channel)
 
-        request = Request(
-            header=RequestHeader(
+        request = p.Request(
+            header=p.RequestHeader(
                 task_id="", 
                 session_id="",
-                command=CommandId.HostReadyCheck,
+                command=p.CommandId.HostReadyCheck,
                 version=PTSL_VERSION,
                 ), 
             request_body_json="")
 
-        response = stub.SendGrpcRequest(request)
+        print(request)
 
-        print(response)
+        #response = stub.SendGrpcRequest(request)
+
+        #print(response)
