8ac959a402fff722c66b409747346149fb2831c6
Jamie Hardt <jamiehardt@me.com>
2023-01-08 21:21:58 -0800

    Removed dead note

--- ptsl/ops/create_session.py
+++ ptsl/ops/create_session.py
@@ -15,25 +15,8 @@ class CreateSession(Operation):
     def command_id(self):
         return pt.CreateSession
 
-    # FIXME: There's something wrong with the protobuf code here, it's simply
-    # not capturing either is_cloud_project or create_from_template from the
-    # creation call.
-    # def json_messup(self, in_json: str, version=1) -> str:
-    #     def hook(dct: dict):
-    #         dct['is_cloud_project'] = False
-    #         dct['create_from_template'] = False
-
-    #         return dct
-
-    #     transformed = json.loads(in_json, object_hook=hook)
-
-    #     return json.dumps(transformed)
-
-
     def on_empty_response_body(self):
         pass
 
-
     def on_response_body(self, _body):
         pass
-
