d15c8c02b0a582ce7ed759834c2257986e819a27
Jamie Hardt <jamiehardt@me.com>
2023-01-21 13:37:33 -0800

    New build system

--- ptsl/__init__.py
+++ ptsl/__init__.py
@@ -1,10 +1,10 @@
-# py-ptsl
-#
+"""
+py-ptsl
+"""
 
 from .client import Client
 from .engine import Engine, open_engine
 from .errors import CommandError
 
-__VERSION__ = '0.0.1'
-__AUTHOR__ = 'Jamie Hardt'
+__version__ = '0.0.1'
 
