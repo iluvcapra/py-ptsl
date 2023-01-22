7a3320e26d8a67b23d980d0b642bebb9e9a07e5d
Jamie Hardt <jamiehardt@me.com>
2023-01-21 21:15:57 -0800

    flake8 cleanup

--- ptsl/util.py
+++ ptsl/util.py
@@ -11,14 +11,18 @@ from ptsl.PTSL_pb2 import SessionTimeCodeRate, SessionFeetFramesRate, \
     SessionRatePull, SampleRate
 
 
-def timecode_info(session_rate: 'SessionTimeCodeRate') -> Tuple[Fraction, bool]:
+def timecode_info(
+        session_rate: 'SessionTimeCodeRate'
+        ) -> Tuple[Fraction, bool]:
     """
-    For the given :class:`~ptsl.PTSL_pb2.SessionTimeCodeRate` enumeration value,
-    returns a library-agnostic description that can be used for time arithmetic.
+    For the given :class:`~ptsl.PTSL_pb2.SessionTimeCodeRate` enumeration
+    value, returns a library-agnostic description that can be used for time
+    arithmetic.
 
-    The first element in the return :class:`tuple` is the duration of a frame in the given 
-    ``session_rate`` as a fractional number of seconds. The second element is a `bool`
-    which is `True` if the session_rate is drop-frame.
+    The first element in the return :class:`tuple` is the duration of a frame
+    in the given ``session_rate`` as a fractional number of seconds. The
+    second element is a `bool` which is `True` if the ``session_rate`` is
+    drop-frame.
 
     :param session_rate: The session rate value.
     :returns: a Tuple of (`frame duration`, `is drop frame`)
@@ -53,10 +57,12 @@ def timecode_info(session_rate: 'SessionTimeCodeRate') -> Tuple[Fraction, bool]:
 
 def feet_frames_info(feet_frames_rate: 'SessionFeetFramesRate') -> Fraction:
     """
-    For the given :class:`~ptsl.PTSL_pb2.SessionFeetFramesRate` enumeration value,
-    returns a library-agnostic description that can be used for time arithmetic.
+    For the given :class:`~ptsl.PTSL_pb2.SessionFeetFramesRate` enumeration
+    value, returns a library-agnostic description that can be used for time
+    arithmetic.
+
     :param feet_frames_rate: The session feet+frames rate value.
-    :returns: The duration of a frame in the given rate, as a fractional number 
+    :returns: The duration of a frame in the given rate, as a fractional number
     of seconds.
     """
     map_dict = {
@@ -69,18 +75,20 @@ def feet_frames_info(feet_frames_rate: 'SessionFeetFramesRate') -> Fraction:
 
     return Fraction(*map_dict.get(feet_frames_rate))
 
+
 def sample_rate_info(sample_rate: 'SampleRate') -> Optional[int]:
     """
     Get the sample rate for a :class:`~ptsl.PTSL_pb2.SampleRate` as an
-    integer. 
+    integer.
 
-    .. note:: The :attr:`~ptsl.PTSL_pb2.SampleRate.SR_None` value will be returned as a `None`
+    .. note:: The :attr:`~ptsl.PTSL_pb2.SampleRate.SR_None` value will be
+        returned as a `None`
     """
     map_dict = {
         pt.SR_192000: 192000,
         pt.SR_176400: 176400,
         pt.SR_96000: 96000,
-        pt.SR_48000: 48000, 
+        pt.SR_48000: 48000,
         pt.SR_44100: 44100,
         pt.SR_None: None
     }
@@ -90,22 +98,23 @@ def sample_rate_info(sample_rate: 'SampleRate') -> Optional[int]:
 def pull_rate_info(rate_pull: 'SessionRatePull') -> Tuple[int, int]:
     """
     For the given :class:`~ptsl.PTSL_pb2.SessionRatePull` enumeration value,
-    returns a library-agnostic description that can be used for time arithmetic.
+    returns a library-agnostic description that can be used for time
+    arithmetic.
 
     Returns a Tuple where ``retval[0]`` can be multiplied by 4% to give the
-    4% pull value (as when converting 24fps and 25fps playback), and ``retval[1]``
-    can be multiplied by 0.1% to give the 0.1% pull value (as when converting
-    NTSC to integral frame rates).  
+    4% pull value (as when converting 24fps and 25fps playback), and
+    ``retval[1]`` can be multiplied by 0.1% to give the 0.1% pull value (as
+    when converting NTSC to integral frame rates).
 
     :param rate_pull: The rate pull.
     :returns: A Tuple of (:class:`int`, :class:`int`)
     """
     map_dict = {
-        pt.SRP_None: (0,0),
-        pt.SRP_Down01: (0,-1),
-        pt.SRP_Down4: (-1,0),
-        pt.SRP_Down4Down01: (-1,-1),
-        pt.SRP_Down4Up01: (-1,1),
+        pt.SRP_None: (0, 0),
+        pt.SRP_Down01: (0, -1),
+        pt.SRP_Down4: (-1, 0),
+        pt.SRP_Down4Down01: (-1, -1),
+        pt.SRP_Down4Up01: (-1, 1),
         pt.SRP_Up01: (0, 1),
         pt.SRP_Up4: (1, 0),
         pt.SRP_Up4Up01: (1, 1),
@@ -115,7 +124,7 @@ def pull_rate_info(rate_pull: 'SessionRatePull') -> Tuple[int, int]:
         "rate_pull (%i) not recgonized" % rate_pull
 
     return map_dict.get(rate_pull)
-    
+
 
 def macos_to_patform_path(macos_path: str) -> str:
     """
@@ -131,11 +140,10 @@ def macos_to_patform_path(macos_path: str) -> str:
 #     """
 #     Converts a path in the current platform's native
 #     format to a colon-delimited MacOS Classic path.
-    
-#     :param path: A native path. Must be normalized and 
+#
+#     :param path: A native path. Must be normalized and
 #     absolute.
 #     """
-
 #     components = []
 #     head = path
 #     while head != "":
@@ -143,5 +151,3 @@ def macos_to_patform_path(macos_path: str) -> str:
 #         components = [tail] + components
 
 #     # FIXME: Finish this
-    
-        
