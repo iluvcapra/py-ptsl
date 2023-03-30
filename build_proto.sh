de9448dcaea8d509106156cc0e46877c6547b5c4
Jamie Hardt <jamiehardt@me.com>
2023-03-29 21:17:35 -0700

    Fixed build script and seems to work

--- build_proto.sh
+++ build_proto.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
-# thus from https://grpc.io
-
-python3 -m pip install --upgrade pip
+set -x
 python3 -m pip install grpcio
 python3 -m pip install grpcio-tools
-
-python3 -m grpc_tools.protoc --proto_path=. ./unary.proto --python_out=. --grpc_python_out=.
+python3 -m grpc_tools.protoc --proto_path=ptsl PTSL.proto \
+    --python_out=ptsl \
+    --grpc_python_out=ptsl
