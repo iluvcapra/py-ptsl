318a0b7b2e57db4d1c3b26dc3b0ace701a258187
Jamie Hardt <jamiehardt@me.com>
2023-03-29 20:44:08 -0700

    Updated build script with notes

--- build_proto.sh
+++ build_proto.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+# thus from https://grpc.io
+
 python3 -m pip install --upgrade pip
 python3 -m pip install grpcio
 python3 -m pip install grpcio-tools
 
-python -m grpc_tools.protoc --proto_path=. ./unary.proto --python_out=. --grpc_python_out=.
+python3 -m grpc_tools.protoc --proto_path=. ./unary.proto --python_out=. --grpc_python_out=.
