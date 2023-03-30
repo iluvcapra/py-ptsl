9d03b2cd9db045c8b02f18d258824b9217b1880c
Jamie Hardt <jamiehardt@me.com>
2023-03-29 21:39:28 -0700

    Tweaked build script

--- build_proto.sh
+++ build_proto.sh
@@ -4,4 +4,6 @@ python3 -m pip install grpcio
 python3 -m pip install grpcio-tools
 python3 -m grpc_tools.protoc --proto_path=ptsl PTSL.proto \
     --python_out=ptsl \
-    --grpc_python_out=ptsl
+    --grpc_python_out=ptsl \
+    --pyi_out=ptsl
+
