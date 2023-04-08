4ed49e5488307e10e9a61578aae9119e36ecf94a
Jamie Hardt <jamiehardt@me.com>
2023-04-08 13:13:05 -0700

    Moving some documentation around

--- examples/print_tracks.py
+++ examples/print_tracks.py
@@ -1,6 +1,20 @@
 # print_tracks.py
 #
 # This example demonstrates getting a list of tracks.
+#
+# ```sh
+# $  python3 examples/print_tracks.py
+# #: ATTRS : TYPE          : NAME                             :     COLOR : ID
+# 1: *     : RoutingFolder : Folder 2                         : #ff2a2a2a : {00000000-2a000000-f404e1df-f298fd4b}
+# 2: *S    : AudioTrack    : A1                               : #ff2a2a2a : {00000000-2a000000-d4cbe0df-2590e43e}
+# 3: *     : AudioTrack    : A2                               : #ff2a2a2a : {00000000-2a000000-d4cbe0df-ac40203f}
+# 4:       : BasicFolder   : Folder 1                         : #ff2a2a2a : {00000000-2a000000-de01e1df-2d2b4575}
+# 5:  SM   : Vca           : VCA 1                            : #ffbc1e0d : {00000000-2a000000-a301e1df-f690ac51}
+# 6:       : Vca           : VCA 2                            : #ffbc1e0d : {00000000-2a000000-a301e1df-5b0aad51}
+# 7:       : AudioTrack    : B1                               : #ff2a2a2a : {00000000-2a000000-d4cbe0df-d3ae273f}
+# 8:    HX : AudioTrack    : B2                               : #ff2a2a2a : {00000000-2a000000-d4cbe0df-cc3b283f}
+# ```
+
 
 import os
 import sys
