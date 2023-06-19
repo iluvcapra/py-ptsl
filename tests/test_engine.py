5a85fd868964963e6adb8f4b949df02256638b68
Jamie Hardt <jamiehardt@me.com>
2023-06-19 01:10:51 -0700

    Tests WIP

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -3,27 +3,178 @@ from unittest import TestCase
 from unittest.mock import Mock, patch
 from contextlib import contextmanager
 
-from ptsl.PTSL_pb2 import GetPTSLVersionResponseBody
+import ptsl.PTSL_pb2 as pt
 from ptsl.engine import open_engine
 import ptsl.ops as ops
 
 @contextmanager 
-def open_engine_with_mock_client(run_response=Optional[Any]):
+def open_engine_with_mock_client(expected_response=Optional[Any]):
     with patch('ptsl.Client'):
         with open_engine(company_name="none", application_name="none") as engine:
-            if run_response is None:
+            if expected_response is None:
                 yield engine
             else:
                 def give_response(op: ops.Operation):
-                    op.response = run_response
+                    op.response = expected_response
                 
                 with patch.object(engine.client, 'run', new=Mock(side_effect=give_response)):
                     yield engine
 
 class TestEngine(TestCase):
+    """
+    The :class:`TestEngine` test case exercises the :class:`Engine` interface. The client
+    is fully mocked.
+    """
 
     def test_ptsl_version(self):
-        with open_engine_with_mock_client(run_response=GetPTSLVersionResponseBody(version=1)) as engine:
-            self.assertIsNotNone(engine)
+        with open_engine_with_mock_client(
+            expected_response=pt.GetPTSLVersionResponseBody(version=1)) as engine:
             self.assertEqual(engine.ptsl_version(),1)
-            engine.client.run.assert_called()
+
+    def test_create_session(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(engine.create_session(name="test name",
+                                  path="test/to/path",
+                                  file_type=pt.SAF_AIFF,
+                                  sample_rate=pt.SR_96000,
+                                  bit_depth=pt.Bit32Float,
+                                  io_setting=pt.IO_51FilmMix,
+                                  is_interleaved=False))
+
+    def test_create_session_from_template(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.create_session_from_template(
+                    template_group="xyz",                
+                    template_name="template",
+                    name="test",       
+                    path="template/test/path",           
+                    file_type=pt.SAF_AIFF,
+                    sample_rate=pt.SR_176400,
+                    bit_depth=pt.Bit16,
+                    io_setting=pt.IO_51DTSMix,
+                    is_interleaved=True))
+
+    def test_create_aaf(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.create_session_from_aaf(
+                    name="test aaf",
+                    path="test/path/to/aaf",
+                    aaf_path="path/to/my/aaf",
+                    file_type=pt.SAF_WAVE,
+                    sample_rate=pt.SR_88200,
+                    bit_depth=pt.Bit24,
+                    io_setting=pt.IO_StereoMix,
+                    is_interleaved=False))
+
+    def test_open_session(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.open_session(
+                    path="path/to/session"
+                )
+            )
+
+    def test_close_session(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.close_session(save_on_close=True)
+            )
+    
+    def test_save_session(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.save_session()
+            )
+
+    def test_save_session_as(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.save_session_as(path="path/to/session/named",
+                                       name="name")
+            )
+
+    def text_export_session_as_text(self):
+        resp = pt.ExportSessionInfoAsTextResponseBody(session_info="test string")        
+        with open_engine_with_mock_client(expected_response=resp) as engine:
+            info = engine.export_session_as_text(include_clip_list=True,
+                                                 include_file_list=True,
+                                                 include_markers=False,
+                                                 include_plugin_list=True,
+                                                 include_track_edls=True,
+                                                 show_sub_frames=True,
+                                                 track_list_type=pt.AllTracks,
+                                                 include_user_timestamp=False,
+                                                 fade_handling_type=pt.ShowCrossfades,
+                                                 track_offset_options=pt.BarsBeats,
+                                                 text_as_file_format=pt.TextEdit,
+                                                 output_type=pt.ESI_String,
+                                                 output_path=None)
+            self.assertEqual(info,"test_string")
+
+    def test_import_data(self):
+        with open_engine_with_mock_client() as engine:
+
+            session_data = pt.SessionData(
+                audio_options=pt.LinkToSourceAudio,
+                audio_handle_size=100,
+                video_options=pt.LinkToSourceVideo,
+                match_options=pt.MT_ImportAsNewTrack,
+                playlist_options=pt.ImportReplaceExistingPlaylists,
+                track_data_to_import=pt.TrackDataToImport(track_data_preset_path="path/to/preset",
+                                                          clip_gain=False,clips_and_media=True,
+                                                          volume_automation=True),
+                timecode_mapping_units=pt.MapStartTimeCodeTo,
+                timecode_mapping_start_time="01:00:00:00",
+                adjust_session_start_time_to_match_source=False
+            )
+
+            audio_data = pt.AudioData(file_list = ["file_1","/path/to/file_2"],
+                                      audio_operations=pt.CopyAudio,
+                                      destination_path="path/to/destination",
+                                      destination=pt.MD_NewTrack,
+                                      location=pt.ML_Selection
+                                      )
+            self.assertIsNone(
+                engine.import_data(session_path="path/to/import.ptx",
+                                   import_type=pt.Audio,
+                                   session_data=session_data,
+                                   audio_data=audio_data)
+            )
+
+    def test_select_all_clips_on_track(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.select_all_clips_on_track(track_name="yada yada")
+            )
+
+    def test_extend_track_selection(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.extend_selection_to_target_tracks(tracks=["a","b"])
+            )
+
+    def test_trim_to_selection(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.trim_to_selection()
+            )
+
+    def test_create_batch_fades(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.create_batch_fades(preset_name="fades1",adjust_bounds=False)
+            )
+
+    def test_rename_track(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.rename_target_track(old_name="x", new_name="y")
+            )
+
+    def test_rename_selected_clip(self):
+        with open_engine_with_mock_client() as engine:
+            self.assertIsNone(
+                engine.rename_target_clip(clip_name="x", new_name="y", rename_file=True)
+            )
