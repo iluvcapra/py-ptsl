484f8815dd69d300d73de0f27ffe8325908c5c5a
Jamie Hardt <jamiehardt@me.com>
2023-06-19 11:54:15 -0700

    Linting

--- examples/make_session.py
+++ examples/make_session.py
@@ -1,6 +1,7 @@
 # make_session.py
 #
-# This example demonstrates how to create a new file with the `ptsl.Engine` class
+# This example demonstrates how to create a new file with the `
+# ptsl.Engine` class
 
 import sys
 import ptsl
@@ -10,5 +11,6 @@ if len(sys.argv) < 3:
     print(f"Usage: {sys.argv[0]} <path> <session-name>")
     sys.exit(-1)
 
-with ptsl.open_engine(company_name="py-ptsl", application_name=sys.argv[0]) as e:
+with ptsl.open_engine(company_name="py-ptsl",
+                      application_name=sys.argv[0]) as e:
     e.create_session(name=sys.argv[2], path=sys.argv[1])
