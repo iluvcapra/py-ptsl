c53f9579b752a7cbb782c3ee34575f2922c1a3f5
Jamie Hardt <jamiehardt@me.com>
2023-06-18 23:42:46 -0700

    Refactored engine mock

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,25 +1,29 @@
-# from typing import Optional
+from typing import Optional, Any
 from unittest import TestCase
 from unittest.mock import Mock, patch
-
-# import grpc
-
+from contextlib import contextmanager
 
 from ptsl.PTSL_pb2 import GetPTSLVersionResponseBody
 from ptsl.engine import open_engine
 import ptsl.ops as ops
 
+@contextmanager 
+def open_engine_with_mock_client(run_response=Optional[Any]):
+    with patch('ptsl.Client'):
+        with open_engine(company_name="none", application_name="none") as engine:
+            if run_response is None:
+                yield engine
+            else:
+                def give_response(op: ops.Operation):
+                    op.response = run_response
+                
+                with patch.object(engine.client, 'run', new=Mock(side_effect=give_response)):
+                    yield engine
 
 class TestEngine(TestCase):
 
-    def test_get_version(self):
-        def ret_version(op: ops.Operation):
-            op.response = GetPTSLVersionResponseBody(version=1)
-            return
-        
-        with patch('ptsl.Client'):
-            with open_engine(company_name="none", application_name="none") as engine:
-                with patch.object(engine.client, 'run', new=Mock(side_effect=ret_version)):
-                    self.assertIsNotNone(engine)
-                    self.assertEqual(engine.ptsl_version(),1)
-                    engine.client.run.assert_called()
+    def test_ptsl_version(self):
+        with open_engine_with_mock_client(run_response=GetPTSLVersionResponseBody(version=1)) as engine:
+            self.assertIsNotNone(engine)
+            self.assertEqual(engine.ptsl_version(),1)
+            engine.client.run.assert_called()
