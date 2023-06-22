72937a954efd287d2adca380d9ceebc7b9d3abd9
Jamie Hardt <jamiehardt@me.com>
2023-06-21 21:14:38 -0700

    Added copyright notices
    And license key to pyproject.toml

--- ptsl/__init__.py
+++ ptsl/__init__.py
@@ -1,6 +1,10 @@
 """
 Native Python PTSL (Pro Tools Scripting Library) RPC interface
 
+This package (c) 2023 Jamie Hardt. All Rights Reserved.
+Read the LICENSE file distributed with this package to learn about
+your rights to use, modify and redistribute this software package.
+
 This project is a work of Pro Tools enthusiasts and users and is not
 affiliated with Avid.
 
