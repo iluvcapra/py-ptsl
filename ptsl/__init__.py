ece5f5fd7ed7f52052a8a34114b4b69d25792d6b
Jamie Hardt <jamiehardt@me.com>
2023-06-23 19:32:20 -0700

    Update __init__.py
    Changed version to distributable form

--- ptsl/__init__.py
+++ ptsl/__init__.py
@@ -10,10 +10,34 @@ affiliated with Avid.
 
 For more information on this package see the github repository at
 https://github.com/iluvcapra/py-ptsl/
+
+Note on Versions
+----------------
+
+The `__version__` property of this module uses semantic versioning of
+a particular form. Instead of the usual three elements, there are 
+four.
+
+- The first element of the version triple is the PTSL version as 
+  supported by the client.
+- The second element is the major version number of this module.
+
+Changes in the first two elements of the version are breaking changes.
+While the module remains in beta development, breaking changes may 
+occur with any change in version.
+
+- The third element increments with feature enhancements. Support for
+  new versions of Pro Tools will always increment this value.
+- The fourth element increments with bug fixes or modifcations to docs, 
+  build system, etc.
+
+Changes in the remaining final two elements of the version are 
+guaranteed not to be breaking changes past X.1.0.0.
+
 """
 
 from .client import Client
 from .engine import Engine, open_engine
 from .errors import CommandError
 
-__version__ = '0.0a2'
+__version__ = '1.0.1.0'
