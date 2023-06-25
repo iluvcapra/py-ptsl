f405c031cf3886f6b76b33cfd9e85d3c92806620
Jamie Hardt <jamiehardt@me.com>
2023-06-24 18:02:53 -0700

    Autopep8

--- ptsl/ops/operation.py
+++ ptsl/ops/operation.py
@@ -5,49 +5,49 @@ from ptsl import PTSL_pb2 as pt
 Q = TypeVar("Q")
 R = TypeVar("R")
 
-class Operation(Generic[Q,R]):
+
+class Operation(Generic[Q, R]):
     """
     An operation composes a `CommandId` with its request and response type.
     The `Operation` abstract base class will infer the `CommandId` Request
-    and Response type from the class name, and will look up the command ID 
+    and Response type from the class name, and will look up the command ID
     code number based on its class name. These can be overridden in subclasses
     for special cases but most of the time simply naming a class after its
     CommandId enum name and deriving it from `Operation` will be sufficient.
 
-    The client runs `Operation`s with the Client.run() method. 
+    The client runs `Operation`s with the Client.run() method.
     """
     request: Q
     response: R
 
     @classmethod
     def request_body(cls) -> Q:
-        return getattr(pt, cls.__name__ + "RequestBody", None) # type: ignore
+        return getattr(pt, cls.__name__ + "RequestBody", None)  # type: ignore
 
     @classmethod
     def response_body(cls) -> R:
-        return getattr(pt, cls.__name__ + "ResponseBody", None) # type: ignore
+        return getattr(pt, cls.__name__ + "ResponseBody", None)  # type: ignore
 
     @classmethod
     def command_id(cls):
         return getattr(pt, cls.__name__, -1)
 
-
     def __init__(self, *args, **kwargs) -> None:
         rq = self.__class__.request_body()
         if rq is not None:
-            self.request = rq(*args, **kwargs) # type: ignore
+            self.request = rq(*args, **kwargs)  # type: ignore
         else:
-            self.request = None # type: ignore
-   
-        self.response = None # type: ignore
+            self.request = None  # type: ignore
+
+        self.response = None  # type: ignore
         self.status = None
         self.task_id = ""
 
     def json_messup(self, in_json: str) -> str:
         """
-        A shim that can be overriden by subclasses to adapt the json the 
-        Protobuf machinery creates into what Pro Tools expects. Necessary 
-        if the server's request parser is buggy/not doing what the .proto 
+        A shim that can be overriden by subclasses to adapt the json the
+        Protobuf machinery creates into what Pro Tools expects. Necessary
+        if the server's request parser is buggy/not doing what the .proto
         file says it should
 
         :param in_json: json from protobuf's `MessageToJson` method
@@ -57,13 +57,13 @@ class Operation(Generic[Q,R]):
 
     def json_cleanup(self, in_json: str) -> str:
         """
-        A shim that can be overriden by subclases to adapt the json Pro 
-        Tools generates in response to what Protobuf expects. Necessary if 
-        the Pro Tools response JSON is buggy/not doing what the .proto file 
+        A shim that can be overriden by subclases to adapt the json Pro
+        Tools generates in response to what Protobuf expects. Necessary if
+        the Pro Tools response JSON is buggy/not doing what the .proto file
         says it should. (Happens sometimes!)
 
         :param in_json: json from the ResponseBody Pro Tools sent.
-        :returns: the json that will be passed to Protobuf's 
+        :returns: the json that will be passed to Protobuf's
         `json_format.Parse` method.
         """
         return in_json
@@ -72,7 +72,7 @@ class Operation(Generic[Q,R]):
         """
         The client calls this when the server reponds.
         """
-        self.response = None # type: ignore
+        self.response = None  # type: ignore
 
     def on_response_body(self, response_body: R):
         """
