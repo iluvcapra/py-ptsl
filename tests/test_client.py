a29500ea53eed879dd3e30b6a3584762175543a9
Jamie Hardt <jamiehardt@me.com>
2023-06-28 14:12:00 -0700

    flake8 test files

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
-from typing import Optional, Any, cast
+from typing import cast
 from unittest import TestCase
-from unittest.mock import Mock, patch
-from contextlib import contextmanager
+from unittest.mock import patch
 
 from google.protobuf import json_format
 
@@ -17,24 +16,29 @@ class MockPtslStub:
         self.register_connection_run = False
 
     def assert_send_request_called(self):
-        assert self.send_request_called, "SendGrpcRequest was not called"
+        assert self.send_request_called, \
+            "SendGrpcRequest was not called"
 
     def assert_register_connection_run(self):
-        assert self.register_connection_run, "RegisterConnection command was not run"
+        assert self.register_connection_run, \
+            "RegisterConnection command was not run"
 
-    def SendGrpcRequest(self, request: pt.Request, _context = None) -> pt.Response:
+    def SendGrpcRequest(
+            self,
+            request: pt.Request,
+            _context=None) -> pt.Response:
         self.send_request_called = True
-        response_body_json = None 
+        response_body_json = None
         if request.header.command == pt.RegisterConnection:
-            response_body_json=json_format.MessageToJson(
+            response_body_json = json_format.MessageToJson(
                 pt.RegisterConnectionResponseBody(session_id=self.session_id)
             )
             self.register_connection_run = True
 
         return pt.Response(
             header=pt.ResponseHeader(
-                task_id=request.header.task_id, 
-                command=request.header.command, 
+                task_id=request.header.task_id,
+                command=request.header.command,
                 status=pt.Completed,
                 progress=100),
             response_body_json=response_body_json,
@@ -50,10 +54,13 @@ class TestClient(TestCase):
 
                 with open_client(company_name='Test',
                                  application_name='Test') as client:
-            
+
                     self.assertIsNotNone(client)
                     self.assertTrue(client.is_open)
                     self.assertEqual(client.session_id, "abc123")
-                    cast(MockPtslStub, client.raw_client).assert_send_request_called()
-                    cast(MockPtslStub, client.raw_client).assert_register_connection_run()
-
+                    cast(
+                        MockPtslStub,
+                        client.raw_client).assert_send_request_called()
+                    cast(
+                        MockPtslStub,
+                        client.raw_client).assert_register_connection_run()
