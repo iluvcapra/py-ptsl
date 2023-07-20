25788a36cde668818516015c7e505beebd98eb0b
Jamie Hardt <jamiehardt@me.com>
2023-07-20 12:46:58 -0700

    Removed big guard

--- ptsl/ops/export_session_info_as_text.py
+++ ptsl/ops/export_session_info_as_text.py
@@ -1,12 +1,6 @@
 from ptsl.ops import Operation
 
 
-# There is a documented bug with this, it won't return a string
-# if you select ESI_string as the output_type
-#
-# https://duc.avid.com/showthread.php?t=422971
 class ExportSessionInfoAsText(Operation):
+    pass
 
-    @classmethod
-    def response_body(cls):
-        return None
