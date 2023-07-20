1d733cfddf6a1da9271332cf7118a14044da52ed
Jamie Hardt <jamiehardt@me.com>
2023-07-20 10:59:54 -0700

    Autopep

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -14,7 +14,7 @@ class TestSessionBuilder(TestCase):
         with patch('ptsl.Client'):
             with open_engine(company_name="none",
                              application_name="none") as engine:
-                
+
                 builder = engine.create_session(name="test",
                                                 path="test/path")
                 builder.aiff_format()
@@ -29,14 +29,14 @@ class TestSessionBuilder(TestCase):
                 self.assertEqual(called_op.request.file_type, pt.SAF_AIFF)
                 self.assertEqual(called_op.request.sample_rate, pt.SR_44100)
                 self.assertEqual(called_op.request.bit_depth, pt.Bit16)
-                self.assertEqual(called_op.request.input_output_settings, 
+                self.assertEqual(called_op.request.input_output_settings,
                                  pt.IO_StereoMix)
 
     def test_create2(self):
         with patch('ptsl.Client'):
             with open_engine(company_name="none",
                              application_name="none") as engine:
-                
+
                 builder = engine.create_session(name="test",
                                                 path="test/path")
                 builder.wave_format()
@@ -51,6 +51,5 @@ class TestSessionBuilder(TestCase):
                 self.assertEqual(called_op.request.file_type, pt.SAF_WAVE)
                 self.assertEqual(called_op.request.sample_rate, pt.SR_48000)
                 self.assertEqual(called_op.request.bit_depth, pt.Bit32Float)
-                self.assertEqual(called_op.request.input_output_settings, 
+                self.assertEqual(called_op.request.input_output_settings,
                                  pt.IO_51SMPTEMix)
-                     
