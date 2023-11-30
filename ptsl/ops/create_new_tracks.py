fc4f6f51996e4bff00c7ef25539ded3d706074c6
Norwin <git@nroo.de>
2023-11-30 01:12:33 +0100

    manually add operations added in 2023.9 for use with client

---
+++ ptsl/ops/create_new_tracks.py
@@ -0,0 +1,5 @@
+from ptsl.ops import Operation
+
+
+class CreateNewTracks(Operation):
+    pass
---
+++ ptsl/ops/edit_tool.py
@@ -0,0 +1,8 @@
+from ptsl.ops import Operation
+
+
+class GetEditTool(Operation):
+    pass
+
+class SetEditTool(Operation):
+    pass
