815ed1867ddd939d23414a5f99353dc60dd6efd3
Jamie Hardt <jamie@squad51.us>
2025-10-08 10:08:48 -0700

    Added to readme

--- examples/tool.bash
+++ examples/tool.bash
@@ -2,22 +2,22 @@
 
 # A collection of example PTSL requests
 
-# poetry run python3 -m ptsl CreateSession <<JSON 
-# {"session_name": "Test Session", 
-# "session_location": "/Users/j/Desktop",
-# "file_type": "FT_WAVE",
-# "sample_rate": "SR_48000",
-# "bit_depth": "Bit24",
-# "input_output_settings": "IO_Last",
-# "is_interleaved": true,
-# "is_cloud_project": false,
-# "create_from_template": false,
-# "template_group": "",
-# "template_name": ""
-# }
-# JSON
+poetry run python3 -m ptsl CreateSession <<JSON 
+{"session_name": "Test Session", 
+"session_location": "/Users/j/Desktop",
+"file_type": "FT_WAVE",
+"sample_rate": "SR_48000",
+"bit_depth": "Bit24",
+"input_output_settings": "IO_Last",
+"is_interleaved": true,
+"is_cloud_project": false,
+"create_from_template": false,
+"template_group": "",
+"template_name": ""
+}
+JSON
 
-poetry run python3 -m ptsl GetTrackList <<JSON 
+poetry run python3 -m ptsl GetTrackList <<JSON
 {"pagination_request": {
   "limit": 100, "offset": 0
   },
